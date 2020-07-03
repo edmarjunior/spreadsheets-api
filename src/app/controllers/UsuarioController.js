60d5f4163e3397c4fbf0007647b4c59d8ba3164a
Edmar Costa <edmar.acjr@gmail.com>
2020-07-03 20:26:33 -0300

    Retornando dados do time no endpoint de listagem de usuários

--- src/app/controllers/UsuarioController.js
+++ src/app/controllers/UsuarioController.js
@@ -1,9 +1,17 @@
 import Usuario from "../models/Usuario";
+import Time from "../models/Time";
 
 class UsuarioController {
     async index(req, res) {
         const usuarios = await Usuario.findAll({
-            attributes: ['id', 'nome', 'email']
+            attributes: ['id', 'nome', 'email'],
+            include: [
+                {
+                    model: Time,
+                    as: 'time',
+                    attributes: ['id', 'nome', 'cor_hexa']
+                }
+            ]
         });
         return res.json(usuarios);
     }
