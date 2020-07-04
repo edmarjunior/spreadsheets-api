27d536d93b9638e58aa8301d8e0c8d576455883c
Edmar Costa <edmar.acjr@gmail.com>
2020-07-04 18:06:01 -0300

    Adicionando validações e formatações na listagem de apontamentos

--- src/app/controllers/ApontamentoController.js
+++ src/app/controllers/ApontamentoController.js
@@ -1,6 +1,7 @@
+import { format } from "date-fns";
+
 import Apontamento from '../models/Apontamento';
 import ApontamentoAnalistas from '../models/ApontamentoAnalistas';
-import { attrs } from 'factory-girl';
 
 class ApontamentoControler {
     async index(req, res) {
@@ -11,52 +12,68 @@ class ApontamentoControler {
             ano,
         } = req.query;
 
-        const idsAnalistas = rangeAnalistas === 'todos' ? [] : rangeAnalistas.split(',').map(id => +id);
-
-        console.log(rangeAnalistas);
-        console.log(idsAnalistas);
-
-        let apontamentosAnalistas;
-        
-        if (rangeAnalistas === 'todos') {
-            apontamentosAnalistas = await ApontamentoAnalistas.findAll({
-                include: [
-                    {
-                        model: Apontamento,
-                        as: 'apontamento',
-                        attributes: ['id', 'range_analistas', 'assunto', 'descricao', 'indicador_aprovacao']
-                    }
-                ]
-            }); 
-        } else {
-            apontamentosAnalistas = await ApontamentoAnalistas.findAll({
-                include: [
-                    {
-                        model: Apontamento,
-                        as: 'apontamento',
-                        attributes: ['id', 'range_analistas', 'assunto', 'descricao', 'indicador_aprovacao']
-                    }
-                ],
-                where: {
-                    id_analista: idsAnalistas 
-                }
-            }); 
+        if (!rangeAnalistas) {
+            return res.status(400).json({message: 'Favor informar algum analista'})
         }
 
-        let apontamentosDistinct = [];
+        if (!rangeSituacoes) {
+            return res.status(400).json({message: 'Favor informar algum status'})
+        }
 
-        apontamentosAnalistas.forEach(apontamento => {
-            if (apontamentosDistinct.length && apontamentosDistinct.find(ap => ap.id === apontamento.apontamento.id)) {
+        if (!mes || !ano) {
+            return res.status(400).json({message: 'Favor informar o mês e ano'})
+        }
+
+        const idsAnalistas = rangeAnalistas === 'todos' ? [] : rangeAnalistas.split(',').map(id => +id);
+        
+        const apontamentosAnalistas = await ApontamentoAnalistas.findAll({
+            include: [
+                {
+                    required: true,
+                    model: Apontamento,
+                    as: 'apontamento',
+                    where: {
+                        mes,
+                        ano,
+                    }
+                },
+            ],
+            where: rangeAnalistas === 'todos' ? {} : { id_analista: idsAnalistas }
+        }); 
+
+        let apontamentosDistinct = [];
+        let contador = 0;
+
+        apontamentosAnalistas.forEach(apontamentoAnalista => {
+            let { apontamento, apontamento: { indicador_aprovacao } } = apontamentoAnalista;
+
+            if (indicador_aprovacao) {
+                indicador_aprovacao = indicador_aprovacao.toUpperCase();
+            }
+
+            if (rangeSituacoes !== 'todos') {
+                if ((rangeSituacoes.includes('1,2') && !indicador_aprovacao) ||
+                    (rangeSituacoes.includes('1,3') && indicador_aprovacao === 'N') ||
+                    (rangeSituacoes.includes('2,3') && indicador_aprovacao === 'S') ||
+                    (rangeSituacoes === '1' && indicador_aprovacao !== 'S') ||
+                    (rangeSituacoes === '2' && indicador_aprovacao !== 'N') ||
+                    (rangeSituacoes === '3' && !!indicador_aprovacao)) {
+                    return;
+                }
+            }
+
+            if (apontamentosDistinct.length && apontamentosDistinct.find(ap => ap.id === apontamento.id)) {
                 return;
             }
 
             apontamentosDistinct.push({
-                range_analistas: apontamento.apontamento.range_analistas,
-                assunto: apontamento.apontamento.assunto,
-                descricao: apontamento.apontamento.descricao,
-                indicador_aprovacao: apontamento.apontamento.indicador_aprovacao,
+                ...apontamento.dataValues,
+                data_solicitacao: format(apontamento.data_solicitacao, 'dd/MM/yyyy'),
+                inicio: format(apontamento.inicio, 'dd/MM/yyyy'),
+                termino: format(apontamento.termino, 'dd/MM/yyyy'),
+                contador: ++contador,
             });
-        })
+        });
 
         return res.json(apontamentosDistinct);
     }
