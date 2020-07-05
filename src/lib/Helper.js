357e1792b5c6697c129107bc04a2f3e989ae6808
Edmar Costa <edmar.acjr@gmail.com>
2020-07-05 11:19:57 -0300

    Criação do método convertToNumericMonth

--- src/lib/Helper.js
+++ src/lib/Helper.js
@@ -31,3 +31,23 @@ export function sum(array, coluna) {
 
     return total.toFixed(2);
 }
+
+export const convertToNumericMonth = (monthLiteral) => {
+    monthLiteral = monthLiteral.toLowerCase();
+    switch(monthLiteral) {
+        case 'janeiro': return 1;
+        case 'fevereiro': return 2;
+        case 'março': return 3;
+        case 'marco': return 3;
+        case 'abril': return 4;
+        case 'maio': return 5;
+        case 'junho': return 6;
+        case 'julho': return 7;
+        case 'agosto': return 8;
+        case 'setembro': return 9;
+        case 'outubro': return 10;
+        case 'novembro': return 11;
+        case 'dezembro': return 12;
+        default: return null;
+    }
+}
